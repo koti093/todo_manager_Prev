3bfc7c9366919a22e49f81de9107c286eca95fd7
koti093 <koti093@gmail.com>
2021-10-02 11:57:54 +0530

    Add ability to create & update & show & login a user

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,12 @@ Rails.application.routes.draw do
   #post "todos/create", to: "todos#create"
   #post "todos/update", to: "todos#update"
   #get "todos/:id", to: "todos#show"
-  get "users", to: "users#index"
-  get "users/create", to: "users#create"
-  get "users/update", to: "users#update"
-  get "users/:id", to: "users#show"
+  #get "users", to: "users#index"
+  #post "users/create", to: "users#create"
+  #post "users/update", to: "users#update"
+  #get "users/login", to: "users#login"
+  #get "users/:id", to: "users#show"
 
-  #resources :todos
-  #resources :users
+  resources :todos
+  resources :users
 end
