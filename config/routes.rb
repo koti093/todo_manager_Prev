466463db5bb5b2ef2820f4ed8edf423cc18f1f83
koti093 <koti093@gmail.com>
2021-10-02 10:43:39 +0530

    Add ability to update a Todo complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   #get "todos", to: "todos#index"
   #post "todos/create", to: "todos#create"
+  #post "todos/update", to: "todos#update"
   #get "todos/:id", to: "todos#show"
+
   resources :todos
 end
